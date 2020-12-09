7bf484a9009cc7bfebcf2fffcc37401fb4bb98cf
ethramos22 <59628613+ethramos22@users.noreply.github.com>
2020-12-09 01:49:56 -0700

    added comments

--- data/data_retrieval.py
+++ data/data_retrieval.py
@@ -7,10 +7,13 @@ session = Session(aws_access_key_id=cfg.myconfig["AccessKey"],
                 
 s3 = session.resource('s3')
 
+# Our bucket on Amazon s3 that contains our csv files of our data
 my_bucket = s3.Bucket('xitches')
 
 for s3_files in my_bucket.objects.all():
+    # If our file is an icon png then add it to a seperate folder named icons
     if "icon" in s3_files.key:
         s3.meta.client.download_file('xitches',s3_files.key,"icons/{}".format(s3_files.key))
+    # Else download the file onto our current directory    
     else:
         s3.meta.client.download_file('xitches',s3_files.key,s3_files.key)
